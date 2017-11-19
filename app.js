3b9f3f977ae059eda00909d3412ac04b74030a32
Alice Freda <alicefreda@gmail.com>
2017-11-19 19:16:39 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ var express = require('express')
 var app = express()
 
 // Load Controllers
-
+const SiteController = require("./controllers/SiteController.js")
 
 // Routes
+app.get("/", SiteController.Index)
+app.get("/about", SiteController.About)
+app.get("/contact", SiteController.Contact)
 
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,16 @@
 const SiteController = {};
 
+//Index
+SiteController.Index = function(req, res) {
+  res.render("/")
+}
+//About
+SiteController.About = function(req, res) {
+  res.render("/about")
+}
+//Contact
+SiteController.Contact = function(req, res) {
+  res.render("/contact")
+}
 
 module.exports = SiteController;
